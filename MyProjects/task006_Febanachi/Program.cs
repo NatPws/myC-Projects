251dd6b8c10e88e39a768e2d89d99072b6ae4f1b
Natalia <natalia.ab.ca@gmail.com>
2023-12-17 23:29:48 +0300

    task006, DONE

---
+++ MyProjects/task006_Febanachi/Program.cs
@@ -0,0 +1,10 @@
+﻿double Fibonacci(int n)  // изначально вводилось int, но оно для маленьких чисел
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine(Fibonacci(i));
+}
